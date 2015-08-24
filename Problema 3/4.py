014d1db9dec28286199dc06c581070f2feec8b0f
Gastón Haro <gaston.haro@fing.edu.uy>
2015-08-24 00:21:44 -0300

    Ej 4 terminado.

--- Problema 3/4.py
+++ Problema 3/4.py
@@ -2,9 +2,13 @@
 import subprocess
 import re
 
-#dominio = raw_input('Ingrese un nombre de dominio: ')
-dominio = "google.com"
-#IP = raw_input('Ingrese una direccion IP: ')
+testing = 0
+if testing:
+	dominio = "google.com"
+	IP = "74.125.141.26"
+else:
+	dominio = raw_input('Ingrese un nombre de dominio: ')
+	IP = raw_input('Ingrese una direccion IP: ')
 
 dig = subprocess.Popen(["dig", dominio, "NS", "+noadditional", "+nostats"], stdout=subprocess.PIPE)
 salida = dig.communicate()[0]
@@ -16,10 +20,10 @@ if noError.search(salida):
 	ns = re.compile('\s+NS\s+([\w|\.]*)')
 
 	if ns.search(salida):
+		authorities = []
 		for i in ns.findall(salida):
 			if i != '':
-				authority = i
-				break
+				authorities = authorities + [i]
 	else:
 		print "Error desconocido."
 
@@ -30,13 +34,20 @@ elif notADomain.search(salida):
 else:
 	print "\nError desconocido.\nVuelva a intentarlo con otro dominio."
 	
-authorityCmd = "@" + authority
-dig = subprocess.Popen(["dig", authorityCmd, dominio, "MX", "+noall", "+additional"], stdout=subprocess.PIPE)
-salida = dig.communicate()[0]
+ips = []
+for i in authorities:
+	authorityCmd = "@" + i
+	dig = subprocess.Popen(["dig", authorityCmd, dominio, "MX", "+noall", "+additional"], stdout=subprocess.PIPE)
+	salida = dig.communicate()[0]
 
-print "\nSe consulto al servidor autoritativo: " + authority +"\n"
-ips = re.findall('(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', salida)
-
-#encontre = re.compile(IP)
+	ipsAux = re.findall('(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', salida)
+	for i in ipsAux:
+		if not i in ips:
+			ips = ips + [i]
 for i in ips:
 	print i
+
+if IP in ips:
+	print "HURRAY"
+else:
+	print "noooRRAY"
