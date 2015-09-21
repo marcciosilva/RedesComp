29d1a055763ce5e8f75ff57e5b6f25e7f1ef0591
Marccio Silva <marccio.silva@fing.edu.uy>
2015-09-21 00:43:25 -0300

    Server agregar clientes a un hash, y envía mensaje de respuesta

--- Obligatorio 2/Servidor/src/main.cpp
+++ Obligatorio 2/Servidor/src/main.cpp
@@ -1,29 +1,127 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include<map>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <regex.h>
+
+using namespace std;
+
+map<string, struct sockaddr_in> clientes;
+int sockfd, n;
+struct sockaddr_in servaddr, cliaddr;
+socklen_t len;
+char mesg[1000];
+
+bool esLogin() {
+    char* regexpLogin = "LOGIN\\s(\\w+)";
+    //distingo entre tipos de mensaje
+    int i = 0;
+    int res;
+    int len;
+    char result[BUFSIZ];
+    char err_buf[BUFSIZ];
+    //        char* src = "hello world";
+    //    const char* pattern = "(\\w+)";
+    regex_t preg;
+
+    regmatch_t pmatch[10];
+
+    if ((res = regcomp(&preg, regexpLogin, REG_EXTENDED)) != 0) {
+        regerror(res, &preg, err_buf, BUFSIZ);
+        printf("regcomp: %s\n", err_buf);
+        exit(res);
+    }
+
+    res = regexec(&preg, mesg, 10, pmatch, REG_NOTBOL);
+
+    if (res == REG_NOMATCH) {
+        printf("No es mensaje de login\n");
+        return false;
+    } else return true;
+    regfree(&preg);
+    return true;
+}
+
+bool apodoDisponible() {
+    //si el apodo está disponible lo agrega al map de clientes
+    char* regexpLogin = "LOGIN\\s(\\w+)";
+    //distingo entre tipos de mensaje
+    int i = 0;
+    int res;
+    int len;
+    char result[BUFSIZ];
+    char err_buf[BUFSIZ];
+    //        char* src = "hello world";
+    //    const char* pattern = "(\\w+)";
+    regex_t preg;
+
+    regmatch_t pmatch[10];
+
+    if ((res = regcomp(&preg, regexpLogin, REG_EXTENDED)) != 0) {
+        regerror(res, &preg, err_buf, BUFSIZ);
+        printf("regcomp: %s\n", err_buf);
+        exit(res);
+    }
+
+    res = regexec(&preg, mesg, 10, pmatch, REG_NOTBOL);
+
+    if (res == REG_NOMATCH) {
+        printf("Mensaje invalido\n");
+        return false;
+    }
+    if (pmatch[1].rm_so != -1) {
+        len = pmatch[i].rm_eo - pmatch[i].rm_so;
+        memcpy(result, mesg + pmatch[i].rm_so, len);
+        result[len] = 0;
+        // chequeo que el nombre no esté en uso
+        if (clientes.find(result) == clientes.end()) {
+            string tmpCliente(result);
+            clientes[tmpCliente] = cliaddr;
+            return true;
+        } else return false;
+    } else return false;
+    regfree(&preg);
+    return true;
+}
 
 int main(int argc, char**argv) {
-	int sockfd, n;
-	struct sockaddr_in servaddr, cliaddr;
-	socklen_t len;
-	char mesg[1000];
 
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(54321);
-	bind(sockfd, (struct sockaddr *) &servaddr, sizeof (servaddr));
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	printf("Servidor iniciado\n\n");
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(54321);
+    bind(sockfd, (struct sockaddr *) &servaddr, sizeof (servaddr));
 
-	while (true) {
-		len = sizeof (cliaddr);
-		n = recvfrom(sockfd, mesg, 1000, 0, (struct sockaddr *) &cliaddr, &len);
-		sendto(sockfd, mesg, n, 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
-		mesg[n] = 0;
-		printf("Mensaje recibido:\n");
-		printf(mesg);
-		printf("--Fin--\n\n");
-	}
+    cout << "Servidor iniciado\n\n";
+
+    while (true) {
+
+        len = sizeof (cliaddr);
+        n = recvfrom(sockfd, mesg, 1000, 0, (struct sockaddr *) &cliaddr, &len);
+
+        if (esLogin()) {
+            if (apodoDisponible()) {
+                strncpy(mesg, "", 1000);
+                cout << "Nombre disponible" << endl; //nombre disponible
+                strncpy(mesg, "OK", 2);
+            } else {
+                strncpy(mesg, "", 1000);
+                cout << "Nombre no disponible" << endl; //nombre no disponible, ya en uso
+                strncpy(mesg, "NOK", 3);
+            }
+        }
+
+        sendto(sockfd, mesg, n, 0, (struct sockaddr *) &cliaddr, sizeof (cliaddr));
+        mesg[n] = 0; //asumo que deja en null todos los caracteres
+        cout << "Mensaje recibido:\n";
+        cout << mesg;
+        cout << "--Fin--\n\n";
+    }
 }
