d784097574da45657640c22059afd00f5b147a6e
Marccio Silva <marccio.silva@fing.edu.uy>
2015-10-15 02:45:59 -0300

    Reimplementado rdt 3.0 para cliente unicast *ConfiabilidadUnicast pasó a ser sólo una clase de utilidades para confiabilidad (is_ACK, make_pkt, etc.) *Sólo hay un thread de envio unicast vivo a la vez, que se duerme hasta que ocurra un timeout o sea interrumpido por haber recibido un ACK que esperaba

--- Obligatorio 2/Cliente/src/redes/EnvioUnicast.java
+++ Obligatorio 2/Cliente/src/redes/EnvioUnicast.java
@@ -1,6 +1,11 @@
 package redes;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Thread para mensajes que no involucran login o escucha de mensajes privados.
@@ -9,9 +14,18 @@ import java.net.InetAddress;
  *
  * @author marccio
  */
-public class EnvioUnicast extends ConfiabilidadUnicast {
+public class EnvioUnicast extends Thread {
 
     String msj;
+    DatagramPacket out_pck;
+    boolean envioExitoso = false; //marca si tengo que terminar ejecución del thread
+    Cliente cliente = Cliente.getInstance();
+    InetAddress serverIP;
+    int serverPort;
+    boolean confiabilidad;
+    public final static int TIMEOUT = 2000;
+    public final static int PACKETSIZE = 65536;
+    protected DatagramSocket socketUnicast;
 
     /**
      * Se construye el thread con el mensaje a enviar y la información del
@@ -31,7 +45,77 @@ public class EnvioUnicast extends ConfiabilidadUnicast {
 
     @Override
     public void run() {
-        socketUnicast = cliente.getUnicastSocket();
-        rdt_send(msj);
+        try {
+            socketUnicast = cliente.getUnicastSocket();
+            rdt_send(msj);
+        } catch (IOException ex) {
+            Logger.getLogger(EnvioUnicast.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    //la única situación en la que se interrumpe al thread es si llegó un ACK
+    @Override
+    public void interrupt() {
+        super.interrupt(); //To change body of generated methods, choose Tools | Templates.
+        envioExitoso = true;
+    }
+
+    /**
+     * Envía un mensaje (String) aplicando confiabilidad
+     *
+     * @param msj Mensaje a enviar
+     * @throws java.io.IOException
+     */
+    public void rdt_send(String msj) throws IOException {
+        if (!confiabilidad) {
+            byte[] data = msj.getBytes();
+            DatagramPacket paquete = new DatagramPacket(data, data.length, serverIP, serverPort);
+            try {
+                socketUnicast.send(paquete); //es thread safe, no hay que sincronizarlo
+            } catch (IOException ex) {
+                Logger.getLogger(EnvioUnicast.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println(ex.toString());
+            }
+        } else {
+            while (!envioExitoso) {
+                if (cliente.estadoSender == Cliente.EstadoSender.ESPERO_DATA_0) {
+                    try {
+                        byte[] data = msj.getBytes();
+                        DatagramPacket paquete = new DatagramPacket(data, data.length, serverIP, serverPort);
+                        out_pck = UtilsConfiabilidad.makeDatapkt(false, 0, paquete);
+                        socketUnicast.send(out_pck);
+                        cliente.estadoSender = Cliente.EstadoSender.ESPERO_ACK_0;
+                        synchronized (this) {
+                            wait(TIMEOUT); //espero 5 segundos
+                        }
+                    } catch (InterruptedException ex) {
+                        //si es interrumpido es porque llegó el ACK0
+                        envioExitoso = true;
+                        Logger.getLogger(EnvioUnicast.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else if (cliente.estadoSender == Cliente.EstadoSender.ESPERO_DATA_1) {
+                    try {
+                        byte[] data = msj.getBytes();
+                        DatagramPacket paquete = new DatagramPacket(data, data.length, serverIP, serverPort);
+                        out_pck = UtilsConfiabilidad.makeDatapkt(false, 1, paquete);
+                        socketUnicast.send(out_pck);
+                        cliente.estadoSender = Cliente.EstadoSender.ESPERO_ACK_1;
+                        synchronized (this) {
+                            wait(TIMEOUT); //espero 5 segundos
+                        }
+                    } catch (InterruptedException ex) {
+                        //si es interrumpido es porque llegó el ACK1
+                        envioExitoso = true;
+                        Logger.getLogger(EnvioUnicast.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    if (cliente.estadoSender == Cliente.EstadoSender.ESPERO_ACK_0
+                            || cliente.estadoSender == Cliente.EstadoSender.ESPERO_ACK_1) {
+                        //si saltó el timer entro por acá, sea que espere ACK0 o ACK1
+                        //reenvíos
+                        socketUnicast.send(out_pck);
+                    }
+                }
+            }
+        }
     }
 }
